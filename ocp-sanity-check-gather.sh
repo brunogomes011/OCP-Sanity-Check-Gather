ee8787c28103424a424cf09ac910eb1e051f5c0a
Bruno Gomes <bruno.gomes011@gmail.com>
2025-02-22 20:27:15 +0000

    Improved some logic and error handling

--- ocp-sanity-check-gather.sh
+++ ocp-sanity-check-gather.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
+function build_env_vars {
 
-BASE_DOMAIN=$(oc get dns/cluster -oyaml | grep baseDomain | awk '{print $2}')
-API=api.$BASE_DOMAIN
-API_INT=api-int.$BASE_DOMAIN
-INGRESS_URL="*.apps."$BASE_DOMAIN
-ROUTERS_IPS=$(oc get pod -n openshift-ingress -o=jsonpath="{.items[*]['status.podIP']}")
-DNS_UPSTREAMS_INCLUSTER=$(for POD in $(oc get pod -n openshift-dns -l dns.operator.openshift.io/daemonset-dns=default --no-headers | awk '{print $1}'); do oc exec -n openshift-dns -q $POD -c dns -- cat /etc/resolv.conf| grep nameserver | awk '{print $2}';done | sort | uniq -c | awk '{print $2}')
-ALL_URLS=("$API" "$API_INT" "$INGRESS_URL")
-ALL_OCP_ROUTES=("oauth-openshift.apps.$BASE_DOMAIN" "console-openshift-console.apps.$BASE_DOMAIN" "canary-openshift-ingress-canary.apps.$BASE_DOMAIN" )
-USER_CUSTOM_ATTEMPT=$1
-CONN_ATTEMPT="${USER_CUSTOM_ATTEMPT:=1}"
-TEST_POD_NS=openshift-ingress-operator
-TEST_POD_CONTAINER=ingress-operator
-TEST_POD=$(oc get pod -n $TEST_POD_NS | grep -v NAME | awk '{print $1}')
-TIMEOUT=10
+    BASE_DOMAIN=$(oc get dns/cluster -ojson | jq -r .spec.baseDomain)
+    API=api.$BASE_DOMAIN
+    API_INT=api-int.$BASE_DOMAIN
+    INGRESS_URL="*.apps."$BASE_DOMAIN
+    ROUTERS_IPS=$(oc get pod -n openshift-ingress -l ingresscontroller.operator.openshift.io/deployment-ingresscontroller=default -o=jsonpath="{.items[*]['status.podIP']}")
+    DNS_UPSTREAMS_INCLUSTER=$(for POD in $(oc get pod -n openshift-dns -l dns.operator.openshift.io/daemonset-dns=default --no-headers | awk '{print $1}'); do oc exec -n openshift-dns -q $POD -c dns -- cat /etc/resolv.conf| grep nameserver | awk '{print $2}';done | sort | uniq -c | awk '{print $2}')
+    ALL_URLS=("$API" "$API_INT" "$INGRESS_URL")
+    ALL_OCP_ROUTES=("oauth-openshift.apps.$BASE_DOMAIN" "console-openshift-console.apps.$BASE_DOMAIN" "canary-openshift-ingress-canary.apps.$BASE_DOMAIN" )
+    USER_CUSTOM_ATTEMPT=$1
+    CONN_ATTEMPT="${USER_CUSTOM_ATTEMPT:=1}"
+    TEST_POD_NS=openshift-ingress-operator
+    TEST_POD_CONTAINER=ingress-operator
+    TEST_POD=$(oc get pod -n $TEST_POD_NS | grep -v NAME | awk '{print $1}')
+    TIMEOUT=10
+
+
+}
 
 function check_dns_resolution_inbastion {
 
@@ -39,7 +43,6 @@ function check_dns_resolution_inbastion {
 
 }
 
-
 function check_dns_resolution_per_upstream_incluster  {
 
     for (( ATTEMPT=$CONN_ATTEMPT; ATTEMPT >= 1; ATTEMPT-- ))
@@ -53,7 +56,7 @@ function check_dns_resolution_per_upstream_incluster  {
                 DIG_ANSWER=$(echo $DIG_OUTPUT | tr -s ';' '\n' | grep 'ANSWER SECTION' | awk '{print $7}')
                 DIG_STATUS=$(echo $DIG_OUTPUT | tr -s ';' '\n' | grep 'status:' | cut -d "," -f2 | awk '{print $2}')
                 DIG_TIME=$(echo $DIG_OUTPUT | tr -s ';' '\n' | grep 'Query time' | awk '{print $3 " " $4}')
-                printf "$(date +%Y-%m-%dT%H:%M:%S.%3NZ%:z) | Source POD - $TEST_POD - $TEST_POD_NS | Target URL - $URL | DNS answer - ${DIG_ANSWER:="NOK!"} | DNS server - ${DIG_SERVER:="NOK!"} | Query status - ${DIG_STATUS:="NOK!"} | Query time - ${DIG_TIME:="NOK!"} \n"
+                printf "$(date +%Y-%m-%dT%H:%M:%S.%3NZ%:z) | Source POD $TEST_POD_NS/$TEST_POD | Target URL - $URL | DNS answer - ${DIG_ANSWER:="NOK!"} | DNS server - ${DIG_SERVER:="NOK!"} | Query status - ${DIG_STATUS:="NOK!"} | Query time - ${DIG_TIME:="NOK!"} \n"
                 sleep 0.5
             done
     done
@@ -62,7 +65,6 @@ function check_dns_resolution_per_upstream_incluster  {
     done
 
 }
-
 
 function check_ocp_routes_inbastion {
 
@@ -133,19 +135,26 @@ function check_ocp_routes_routers_incluster {
 
 }
 
-
-printf "Starting the ocp-sanity-check-gather script... All tests are going to run $CONN_ATTEMPT time(s)  \n\n"
+printf "Starting the ocp-sanity-check-gather script... All tests are going to run $CONN_ATTEMPT time(s)  \n"
+printf "Building environment variables \n"
+build_env_vars 2> errors.txt
+if [[ -s errors.txt ]]; then
+    echo "Errors found to build the env vars"
+    cat errors.txt
+    rm -f errors.txt
+    exit 1;
+fi
 printf "DNS resolution check within bastion host  \n"
-check_dns_resolution_inbastion
+check_dns_resolution_inbastion  > output_test.log 
 printf "DNS resolution check within the test pod $TEST_POD \n"
-check_dns_resolution_per_upstream_incluster
+check_dns_resolution_per_upstream_incluster  > output_test.log 
 printf "Route sanity check within bastion host \n"
-check_ocp_routes_inbastion
+check_ocp_routes_inbastion  >> output_test.log 
 printf "Route sanity check within bastion host against router pods \n"
-check_ocp_routes_routers_inbastion
+check_ocp_routes_routers_inbastion  >> output_test.log 
 printf "Route sanity check within test pod $TEST_POD \n"
-check_ocp_routes_incluster
+check_ocp_routes_incluster  >> output_test.log 
 printf "Route sanity check within test pod $TEST_POD against router pods \n"
-check_ocp_routes_routers_incluster
-
+check_ocp_routes_routers_incluster  >> output_test.log 
+rm -f errors.txt
 
